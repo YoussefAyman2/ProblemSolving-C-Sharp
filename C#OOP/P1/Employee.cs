b8df358c28bac22820000020b5fcf80c5473ac2d
Youssef Ayman <109854812+YoussefAyman2@users.noreply.github.com>
2024-04-28 16:47:01 +0300

    Add files via upload

---
+++ C#OOP/P1/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{   enum SecurityPrivilege {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+    internal class Employee 
+    {
+        public int Id { set; get; }
+        public SecurityPrivilege Privilege { get; set; }
+        public decimal Salary { set; get; }
+
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set { gender = value == 'M' ? value : value == 'F' ? value :throw new ArgumentException("Gender must be M or F."); ; }
+        }
+        public HiringDate HireDate { set; get; }
+        public Employee()
+        {
+            
+        }
+        public Employee(int _id, SecurityPrivilege _privilege, decimal _salary,char _gender,HiringDate _HireDate)
+        {
+            Id = _id;Privilege = _privilege; Salary = _salary; Gender = _gender;
+            HireDate = _HireDate;
+        }
+        public override string ToString()
+        {
+            //:C for $
+            return $"ID: {Id}, Security Privilege: {Privilege}, Salary: {Salary:C}, Hire Date: {HireDate.Day}/{HireDate.Month}/{HireDate.Year}, Gender: {Gender}";
+        }
+    }
+}
---
+++ C#OOP/P1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            HiringDate date1 = new HiringDate(10, 3, 2022);
+            HiringDate date2 = new HiringDate(15, 6, 2023);
+            HiringDate date3 = new HiringDate(20, 9, 2024);
+
+            
+            Employee[] EmpArr = new Employee[3];
+            EmpArr[0] = new Employee(101, SecurityPrivilege.DBA, 50000, 'M',date1);
+            EmpArr[1] = new Employee(102, SecurityPrivilege.Guest, 30000, 'F',date2);
+            EmpArr[2] = new Employee(103, SecurityPrivilege.DBA, 60000,'M',date3);
+
+            
+            Array.Sort(EmpArr, (x, y) => DateTime.Compare(new DateTime(x.HireDate.Year, x.HireDate.Month, x.HireDate.Day), new DateTime(y.HireDate.Year, y.HireDate.Month, y.HireDate.Day)));
+
+           
+            foreach (var emp in EmpArr)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+    }
+}
